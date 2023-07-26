e886238b69199fd7ed7adefdf624667759a4acae
Laurana Traulle <lauranatraulle34@gmail.com>
2023-07-26 22:09:48 +0200

    Mise en place

---
+++ src/pages/home.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import Banner from "../components/Banner";
+import Card from "../components/Card";
+
+const Home = () => {
+
+    return (
+    <div>
+      <Banner />
+      <Card />
+    </div>
+  );
+};
+
+export default Home;
