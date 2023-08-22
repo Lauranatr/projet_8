af02e8f94ff69a13f8d9be2cc1f808f1bc713261
Laurana Traulle <lauranatraulle34@gmail.com>
2023-08-22 17:00:16 +0200

    "Banner" terminée, "Card" en cours

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,15 +1,22 @@
 import React from 'react';
-import homebanner from '../assets/homebanner.png'
+import homebanner from '../assets/homeBanner.png'
+import aboutbanner from '../assets/aboutBanner.png'
+import { useLocation } from "react-router";
 
 const Banner = () => {
+   
+    let location = useLocation();
+    
     return (
         <div className='homeBanner'>
-            <img src={homebanner} alt="banner" />
+            <img src={location.pathname === "/a-propos" ? aboutbanner : homebanner}
+        alt="banner" />
                 <div className='opacityBanner'></div>
+                {location.pathname === "/" ? (
                     <div className='textBanner'>
                         <h1>Chez vous, partout et ailleurs</h1>
                     </div>
-                
+                 ) : null}
                 </div>
     );
 };
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import data from '../data/data.json';
+import { Link } from 'react-router-dom';
+
 
 const Card = () => {
     return (
-        <div>
-            
+        <div className='containerCards'>
+            <div className='cards'>
+                {data.map((location, index) =>(
+                    <Link to={"/logements/" + location.id} key={index + " " + location.id}>
+
+                    </Link>
+                ))}
+            </div>
         </div>
     );
 };
