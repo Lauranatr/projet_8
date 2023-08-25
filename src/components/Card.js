5dc5c98192f299843e8fac64ba58a392366852e6
Laurana Traulle <lauranatraulle34@gmail.com>
2023-08-25 09:29:14 +0200

    Cards affichées

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import data from '../data/data.json';
-import { Link } from 'react-router-dom';
 
 
 const Card = () => {
+
+
     return (
         <div className='containerCards'>
+            
             <div className='cards'>
-                {data.map((location, index) =>(
-                    <Link to={"/logements/" + location.id} key={index + " " + location.id}>
-
-                    </Link>
+                {data.map((location, index) =>(  
+                    <article>
+                        <img src={location.cover}
+                        alt={location.title} />
+                        <h2>{location.title}</h2>
+                    </article>  
                 ))}
             </div>
         </div>
